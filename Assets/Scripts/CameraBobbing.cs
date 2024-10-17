89d032d8cf7944405affbd4cd5352559186f188e
artem <qvikhoney@gmail.com>
2024-10-17 21:09:01 +0300

    Added camera bobbing feature. Added sound grass-landing sound effect.

---
+++ Assets/Scripts/CameraBobbing.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Headbob : MonoBehaviour
+{
+    [Header("Configuration")] [SerializeField]
+    private bool _enable = true;
+
+    [SerializeField] [Range(0, 0.1f)] private float _Amplitude = 0.015f;
+    [SerializeField] [Range(0, 30)] private float _frequency = 10.0f;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private Transform _cameraHolder;
+    private readonly float _toggleSpeed = 3.0f;
+    private Vector3 _startPos;
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _startPos = _camera.localPosition;
+    }
+
+    private void Update()
+    {
+        if (!_enable) return;
+        CheckMotion();
+        ResetPosition();
+        _camera.LookAt(FocusTarget());
+    }
+
+    private Vector3 FootStepMotion()
+    {
+        var pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * _frequency) * _Amplitude;
+        pos.x += Mathf.Cos(Time.time * _frequency / 2) * _Amplitude * 2;
+        return pos;
+    }
+
+    private void CheckMotion()
+    {
+        var speed = new Vector3(_controller.velocity.x, 0, _controller.velocity.z).magnitude;
+        if (speed < _toggleSpeed) return;
+        if (!_controller.isGrounded) return;
+        PlayMotion(FootStepMotion());
+    }
+
+    private void PlayMotion(Vector3 motion)
+    {
+        _camera.localPosition += motion;
+    }
+
+    private Vector3 FocusTarget()
+    {
+        var pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y,
+            transform.position.z);
+        pos += _cameraHolder.forward * 30.0f;
+        
+        return pos;
+    }
+
+    private void ResetPosition()
+    {
+        if (_camera.localPosition == _startPos) return;
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 8 * Time.deltaTime);
+    }
+}
